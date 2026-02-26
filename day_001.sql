249b92fde0a856784f0ca1b289188ffff94ebe41
Tiago Gomes <tidgomes@gmail.com>
2026-02-26 11:51:22 -0300

    first commit

---
+++ day_001.sql
@@ -0,0 +1,8 @@
+# To access mysql from the command line: mysql -u root -p <Enter> then <password>
+
+create database chicken_coop;
+create database pet_shop;
+drop database pet_shop;
+use chicken_coop;
+show databases;
+select database(); # This command shows which database you have currently selected
