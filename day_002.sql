6c9645b45f3d0205e4cad1d25c515a9b0794015c
Tiago Gomes <tidgomes@gmail.com>
2026-03-02 15:32:54 -0300

    creating tables

---
+++ day_002.sql
@@ -0,0 +1,13 @@
+# day_002
+# Tipos
+	# Numéricos (não fique apegado a todos os tipos)
+		# INT, SMALLINT, TINYINT, MEDIUMINT, BIGINT, DECIMAL, NUMERIC, FLOAT, DOUBLE, BIT
+	# String
+		# CHAR, VARCHAR, BINARY, VARBINARY, BLOB, TINYBLOB, MEDIUMBLOB, LONGBLOB, TEXT, TINYTEXT, MEDIUMTEXT, LONGTEXT, ENUM
+	# Date:
+		# DATA, DATETIME, TIMESTAMP, TIME, YEAR
+
+create database pet_shop;
+use pet_shop;
+CREATE TABLE cats (name VARCHAR(50), age INT);
+CREATE TABLE dogs (name VARCHAR(50), age INT);
